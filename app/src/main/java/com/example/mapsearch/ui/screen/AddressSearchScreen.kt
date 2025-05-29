18baeb02f6c3b052b0832c45922d24c87b81bfce
Nguyễn Văn Kiên <anhkien7780@gmail.com>
2025-05-29 10:07:04 +0700

    Tinh chỉnh UI - SearchBar

--- app/src/main/java/com/example/mapsearch/ui/screen/AddressSearchScreen.kt
+++ app/src/main/java/com/example/mapsearch/ui/screen/AddressSearchScreen.kt
@@ -1,10 +1,13 @@
 package com.example.mapsearch.ui.screen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,6 +19,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,6 +31,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -98,32 +104,43 @@ fun MapSearchBar(
     onValueChange: (String) -> Unit = {}
 ) {
     var value by remember { mutableStateOf("") }
-    OutlinedTextField(
-        modifier = modifier,
-        value = value,
-        onValueChange = {
-            value = it
-            onValueChange(it)
-        },
-        placeholder = { Text("Enter keyword") },
-        singleLine = true,
-        shape = RoundedCornerShape(100.dp),
-        prefix = {
-            if (loadingUIState != LoadingUIState.Loading)
-                Icon(Icons.Filled.Search, contentDescription = "Search Icon")
-            else
-                CircularProgressIndicator()
-        },
-        suffix = {
-            IconButton(onClick = {
-                value = ""
-                onValueChange("")
-            }) {
-                Icon(Icons.Filled.Clear, contentDescription = "Clear Icon")
+    Box(
+        Modifier.height(60.dp).shadow(5.dp, shape = RoundedCornerShape(100.dp))
+    ) {
+        OutlinedTextField(
+            modifier = modifier.fillMaxSize()
+                .background(color = Color.White),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Transparent,
+                unfocusedBorderColor = Color.Transparent
+            ),
+            value = value,
+            onValueChange = {
+                value = it
+                onValueChange(it)
+            },
+            placeholder = { Text(text = "Enter keyword", modifier = Modifier.alpha(0.5f)) },
+            singleLine = true,
+            shape = RoundedCornerShape(100.dp),
+            prefix = {
+                if (loadingUIState != LoadingUIState.Loading)
+                    Icon(Icons.Filled.Search, contentDescription = "Search Icon")
+                else
+                    CircularProgressIndicator()
+            },
+            suffix = {
+                if (value.isNotEmpty()) {
+                    IconButton(onClick = {
+                        value = ""
+                        onValueChange("")
+                    }) {
+                        Icon(Icons.Filled.Clear, contentDescription = "Clear Icon")
+                    }
+                }
             }
-        }
 
-    )
+        )
+    }
 }
 
 @Composable
